f2d1ef66c26fee940ff1b3ee7d73c8bae5409db7
Jester Adduru <jesteradduru@gmail.com>
2023-11-30 11:03:18 +0800

    Preserve group state

--- resources/js/Pages/Admin/Recruitment/Selection/Components/ApplicantsList.vue
+++ resources/js/Pages/Admin/Recruitment/Selection/Components/ApplicantsList.vue
@@ -11,6 +11,7 @@
       <ol v-if="insider.length !== 0">
         <li v-for="(item) in insider" :key="item.id">
           <Link
+            preserve-state="true"
             :class="{
               'text-dark': applicant_details?.id === item.user.id,
             }" :href="route('admin.recruitment.selection.index', {applicant: item.user.id, job_posting: posting.id})"
@@ -43,6 +44,7 @@
     <ol v-if="outsider.length !== 0">
       <li v-for="(item) in outsider" :key="item.id">
         <Link
+          preserve-state="true"
           :class="{
             'text-dark': applicant_details?.id === item.user.id,
           }" :href="route('admin.recruitment.selection.index', {applicant: item.user.id, job_posting: posting.id})"
@@ -77,6 +79,7 @@
     <ol v-if="props.job_applications.length !== 0">
       <li v-for="(item) in props.job_applications" :key="item.id">
         <Link
+          preserve-state="true"
           :class="{
             'text-dark': applicant_details?.id === item.user.id,
           }" :href="route('admin.recruitment.selection.index', {applicant: item.user.id, job_posting: posting.id})"
