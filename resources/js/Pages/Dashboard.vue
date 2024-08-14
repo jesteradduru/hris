e1ea849a58dc1ad1c34e15ec8a02ce7c286597f8
Jester Adduru <jesteradduru@gmail.com>
2024-08-14 16:42:14 +0800

    Update style of dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,7 +10,7 @@ import logo from '@/Assets/Logo.jpg'
   <AuthenticatedLayout>
     <div class="container-fluid" style="height: 30vh">
       <div class="col d-flex justify-content-center align-items-center">
-        <img :src="logo" class="w-50 rounded-circle border" alt="" />
+        <img :src="logo" class="home-image" alt="" />
       </div>
     </div>
   </AuthenticatedLayout>
